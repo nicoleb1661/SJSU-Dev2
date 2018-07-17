d5a7edd48478ea18dd0b0ab70dad258cdd033653
Alejandro Perez <alex2721ap@gmail.com>
2018-07-16 22:55:59 -0700

    Edited the function names in both .cpp and .hpp

--- firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.cpp
+++ firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.cpp
@@ -4,19 +4,19 @@
 
 //Slave address: 40h for write, 41h for read
 
-bool I2CBackpack::init() //initializes the LCD
+bool I2CBackpack::Init() //initializes the LCD
 {
     //Set slave address: A2-A0 all set to Vss (ground)
     return true;
 }
 
 //Pin configuration requires 4 bit data transfer
-void I2CBackpack::set4BitMode()
+void I2CBackpack::Set4BitMode()
 {
-    write(0x2, 0x0);
+    Write(0x2, 0x0);
 }
 
-void I2CBackpack::clearScreen()
+void I2CBackpack::ClearScreen()
 {
     //Set the command to clear screen:
     //00_00-0000-0001
@@ -24,15 +24,15 @@ void I2CBackpack::clearScreen()
     //Sends 20H to all the DDRAM addresses
     write(0x7F, 0x20);
     //Set the DDRAM address back to 0 returns the display to its original status 
-    returnHome();
+    ReturnHome();
 } 
 
-void I2CBackpack::setPosition(uint8_t row, uint8_t col)
+void I2CBackpack::SetPosition(uint8_t row, uint8_t col)
 {
 
 } 
 
-void I2CBackpack::returnHome()
+void I2CBackpack::ReturnHome()
 {
     //Set the DDRAM address to 0 
     //Return the screen into its original state
@@ -40,73 +40,73 @@ void I2CBackpack::returnHome()
 
 }
 
-void I2CBackpack::printChar()
+void I2CBackpack::PrintChar()
 {
 
 }
 
-void I2CBackpack::displayCursor()
+void I2CBackpack::DisplayCursor()
 {
 
 }
 
-void I2CBackpack::noCursor()  //No cursor will show on the display
+void I2CBackpack::NoCursor()  //No cursor will show on the display
 {
 
 }
 
-void I2CBackpack::blinkChar() //The current character will blink 
+void I2CBackpack::BlinkChar() //The current character will blink 
 {
 
 }
 
-void I2CBackpack::solidChar() //The current character won’t blink
+void I2CBackpack::SolidChar() //The current character won’t blink
 {
 
 }
 
-void I2CBackpack::setLineDisplay(uint8_t lines) //Select 1, 2, or 4 lines
+void I2CBackpack::SetLineDisplay(uint8_t lines) //Select 1, 2, or 4 lines
 {
 
 }
 
-bool I2CBackpack::checkBusyFlag()
+bool I2CBackpack::CheckBusyFlag()
 {
 
     return true;
 } 
 
-void I2CBackpack::displayOn()
+void I2CBackpack::DisplayOn()
 {
 
 }
 
-void I2CBackpack::displayOff()
+void I2CBackpack::DisplayOff()
 {
 
 }
 
-void I2CBackpack::shiftCursorLeft()
+void I2CBackpack::ShiftCursorLeft()
 {
 
 }
 
-void I2CBackpack::shiftCursorRight()
+void I2CBackpack::ShiftCursorRight()
 {
 
 }
 
-void I2CBackpack::setFont()
+void I2CBackpack::SetFont()
 {
     
 }
 
-void I2CBackpack::write(uint8_t address, uint8_t data)
+void I2CBackpack::Write(uint8_t address, uint8_t data)
 {
     writeReg(device_address, address, data);
 }
 
-void I2CBackpack::read(uint8_t address) const
+void I2CBackpack::Read(uint8_t address) const
 {
     readReg(device_address, address);
 }
--- firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.hpp
+++ firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.hpp
@@ -7,31 +7,31 @@ class I2CBackpack
 
     public:
         I2CBackpack();
-        bool init();
-        void set4BitMode();
-        void clearScreen();
-        void setPosition(uint8_t row, uint8_t  col);
-        void returnHome();
-        void printChar();
-        void displayCursor();
-        void noCursor(); //No cursor will appear
-        void blinkChar(); //The current character will blink
-        void solidChar(); //The current character will not blink
-        void setLineDisplay(uint8_t lines); //Select 1, 2, or 4 line display
-        bool checkBusyFlag();//Check busy flag
-        void displayOn();
-        void displayOff();
-        void shiftCursorLeft();
-        void shiftCursorRight();
-        void setFont();
+        bool Init();
+        void Set4BitMode();
+        void ClearScreen();
+        void SetPosition(uint8_t row, uint8_t  col);
+        void ReturnHome();
+        void PrintChar();
+        void DisplayCursor();
+        void NoCursor(); //No cursor will appear
+        void BlinkChar(); //The current character will blink
+        void SolidChar(); //The current character will not blink
+        void SetLineDisplay(uint8_t lines); //Select 1, 2, or 4 line display
+        bool CheckBusyFlag();//Check busy flag
+        void DisplayOn();
+        void DisplayOff();
+        void ShiftCursorLeft();
+        void ShiftCursorRight();
+        void SetFont();
         ~I2CBackpack();
 
     private:
         uint8_t deviceAddress;
         uint8_t col;
         uint8_t row;
-        void write(uint8_t address, uint8_t data);
-        void read(uint8_t address)const;
+        void Write(uint8_t address, uint8_t data);
+        void Read(uint8_t address)const;
 
 };
 
