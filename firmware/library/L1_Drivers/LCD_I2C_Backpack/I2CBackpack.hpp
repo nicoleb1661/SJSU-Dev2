6ec8b11efb808f0b26c054e1e12266db4f0cfb11
NicoleB <Triforce@Nicoles-MacBook-Pro.local>
2018-07-16 23:10:09 -0700

    Added override to functions and inherited Interface class

--- firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.hpp
+++ firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.hpp
@@ -2,34 +2,34 @@
 #define I2CBackpack_H
 #include<stdint.h>
 
-class I2CBackpack
+class I2CBackpack : public Interface
 {
 
     public:
         I2CBackpack();
-        bool Init();
-        void Set4BitMode();
-        void ClearScreen();
-        void SetPosition(uint8_t row, uint8_t  col);
-        void ReturnHome();
-        void PrintChar();
-        void DisplayCursor();
-        void NoCursor(); //No cursor will appear
-        void BlinkChar(); //The current character will blink
-        void SolidChar(); //The current character will not blink
-        void SetLineDisplay(uint8_t lines); //Select 1, 2, or 4 line display
-        bool CheckBusyFlag();//Check busy flag
-        void DisplayOn();
-        void DisplayOff();
-        void ShiftCursorLeft();
-        void ShiftCursorRight();
-        void SetFont();
+        bool Init() override;
+        void Set4BitMode() override;
+        void ClearScreen() override;
+        void SetPosition(uint8_t row, uint8_t  col) override;
+        void ReturnHome() override;
+        void PrintChar() override;
+        void DisplayCursor() override;
+        void NoCursor() override; //No cursor will appear
+        void BlinkChar() override; //The current character will blink
+        void SolidChar() override; //The current character will not blink
+        void SetLineDisplay(uint8_t lines) override; //Select 1, 2, or 4 line display
+        bool CheckBusyFlag() override;//Check busy flag
+        void DisplayOn() override;
+        void DisplayOff() override;
+        void ShiftCursorLeft() override;
+        void ShiftCursorRight() override;
+        void SetFont() override;
         ~I2CBackpack();
 
     private:
-        uint8_t deviceAddress;
-        uint8_t col;
-        uint8_t row;
+        uint8_t device_address_;
+        uint8_t col_;
+        uint8_t row_;
         void Write(uint8_t address, uint8_t data);
         void Read(uint8_t address)const;
 
