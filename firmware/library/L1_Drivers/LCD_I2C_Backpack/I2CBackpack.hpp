a78de5489abf8180360c32daa35725b57092fe73
NicoleB <Triforce@Nicoles-MacBook-Pro.local>
2018-07-18 21:43:24 -0700

    Merged Interface.hpp and I2CBackpack.hpp

--- firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.hpp
+++ firmware/library/L1_Drivers/LCD_I2C_Backpack/I2CBackpack.hpp
@@ -1,35 +1,53 @@
-#include "Interface.hpp"
+#pragma once
 
-#ifndef I2CBackpack_H
-#define I2CBackpack_H
+#include <cstdint>
 
+//Import SJOne libraries to be used for testing
+#include "L2_Drivers/base/i2c_base.hpp"
 
-class I2CBackpack : public Interface 
+class BackpackInterface
+{
+public:
+    virtual void Init();
+    virtual void Set4BitMode();
+    virtual void ClearScreen();
+    virtual void SetPosition(uint8_t row, uint8_t col);
+    virtual void ReturnHome();
+    virtual void PrintChar();
+    virtual void CursorControl(bool show_cursor, bool blink_cursor);
+    virtual void SetLineDisplay(uint8_t lines);
+    virtual bool CheckBusyFlag();
+    virtual void DisplayControl();
+    virtual void ShiftCursor();
+    virtual void SetFont();
+}
+
+class I2CBackpack : public BackpackInterface
 {
 
-    public:
-        I2CBackpack();
-        bool Init() override;
-        void Set4BitMode() override; 
-        void ClearScreen() override;
-        void SetPosition(uint8_t row, uint8_t  col) override;
-        void ReturnHome() override;
-        void PrintChar() override;
-        void CursorControl(bool show_cursor, bool blink_cursor) override; //Replaces NoCursor DisplayCursor, BlinkChar, and SolidChar
-        void SetLineDisplay(uint8_t lines) override; //Select 1, 2, or 4 line display
-        bool CheckBusyFlag() override;//Check busy flag
-        void DisplayControl() override;
-        void ShiftCursor() override;
-        void SetFont() override;
-        ~I2CBackpack();
+public:
+    I2CBackpack();
+    bool Init() override;
+    void Set4BitMode() override;
+    void ClearScreen() override;
+    void SetPosition(uint8_t row, uint8_t  col) override;
+    void ReturnHome() override;
+    void PrintChar() override;
+    void CursorControl(bool show_cursor, bool blink_cursor) override; //Replaces NoCursor DisplayCursor, BlinkChar, and SolidChar
+    void SetLineDisplay(uint8_t lines) override; //Select 1, 2, or 4 line display
+    bool CheckBusyFlag() override;//Check busy flag
+    void DisplayControl() override;
+    void ShiftCursor() override;
+    void SetFont() override;
+    ~I2CBackpack();
 
-    private:
-        uint8_t device_address_;
-        uint8_t col_;
-        uint8_t row_;
-        void Write(uint8_t address, uint8_t data);
-        void Read(uint8_t address)const;
+private:
+    uint8_t device_address_;
+    uint8_t col_;
+    uint8_t row_;
+    void Write(uint8_t address, uint8_t data);
+    void Read(uint8_t address)const;
 
 };
 
-#endif
+
